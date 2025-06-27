8dcac9becda982aeede90bd39f124a7f8bf30396
Adonpm <adon.pmpm@gmail.com>
2025-06-27 10:35:59 +0530

    Rag pipeline is setup

--- backend/app/workflows/rag_pipeline.py
+++ backend/app/workflows/rag_pipeline.py
@@ -1,7 +1,9 @@
 from backend.app.services.pdf_utils import extract_data_from_pdf
+from backend.app.services.vector_store import get_vector_store, add_documents, get_retriever
 from backend.app.core.chunker import chunk_documents_semantic, chunk_documents_recursive
+from backend.app.services.rag_chain import rag_chain
 
-def rag_pipeline(file_path:str, use_semantic:bool = False):
+def rag_pipeline(file_path:str, use_semantic:bool = False, user_input="What is autocad"):
     print("[STEP 1] Extracting document pages...")
     docs = extract_data_from_pdf(file_path)
     print(f"[INFO] Total pages extracted: {len(docs)}")
@@ -11,11 +13,20 @@ def rag_pipeline(file_path:str, use_semantic:bool = False):
         chunks = chunk_documents_semantic(docs)
     else:
         chunks = chunk_documents_recursive(docs)
-
     print(f"[INFO] Total chunks created: {len(chunks)}")
     print(f"[PREVIEW] First chunk:\n{chunks[0].page_content[:300]}...\n")
 
-    return chunks
+    print(f"[STEP 3] Setting up vector store and retrievel pipeline...")
+    vector_store = get_vector_store()
+    add_documents(vector_store, chunks)
+    retriever = get_retriever(vector_store)
 
+    print(f"[STEP 4] Setting up RAG Chain...")
+    chain = rag_chain(retriever)
+
+    print(f"[STEP 5] Querying user input...")
+    chain.invoke(user_input)
+    print(chain.invoke(user_input))
+    
 if __name__ == "__main__":
     rag_pipeline(r"D:\PERSONAL\Agentic-AI-Masterclass\2. Langchain Basics\data\llama2.pdf", use_semantic=False)
